647034b0a036df114613d683e317b99e336e3ce5
Abraham Peterkin <abraham_peterkin@brown.edu>
2018-04-28 19:26:31 -0400

    changed scaffolding to use react, added 'load image' button

---
+++ src/components/PreviewBar/PreviewBar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import PreviewImage from './PreviewImage'
+
+class PreviewBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: 0
+    }
+  }
+
+  onImageClick(index) {
+    this.setState({
+      selected: index
+    })
+  }
+
+  render() {
+    const { images } = this.props
+    const { selected } = this.state
+    return (
+      <div className='preview-bar'>
+        {images.map((image, index) => {
+          return (
+            <PreviewImage
+              key={index}
+              index={index}
+              isSelected={index === selected}
+              path={image.path}
+              onImageClick={this.onImageClick.bind(this)}
+            />
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+PreviewBar.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default PreviewBar
